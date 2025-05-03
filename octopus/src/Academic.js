89a120516f071d230873f97cb9a5661c7266764b
Vaidehi Srinivas <vaidehi.8913@gmail.com>
2025-05-03 11:23:50 -0500

    colt link

--- octopus/src/Academic.js
+++ octopus/src/Academic.js
@@ -265,7 +265,7 @@ class Academic extends Component {
                 <b>Computing High-Dimensional Confidence Sets for Arbitrary Distributions</b> 
                     &nbsp;<a href="https://arxiv.org/abs/2504.02723">[arXiv]</a><br/>
                 <i>with <a href="https://www.stat.uchicago.edu/~chaogao/">Chao Gao</a>, <a href="https://lirenshan.github.io/">Liren Shan</a>, <a href="https://users.cs.northwestern.edu/~aravindv/">Aravindan Vijayaraghavan</a>,</i><br/>
-                COLT 2025. 
+                <a href="https://learningtheory.org/colt2025/">COLT 2025</a>. 
             </div>;
 
         var highDimConformalPaperDescription = 
